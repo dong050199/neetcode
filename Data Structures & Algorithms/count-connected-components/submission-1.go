243e145cd65675817dac8082f1d6816f43581fa7
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-04-05 09:42:16 +0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.go
@@ -0,0 +1,40 @@
+func countComponents(n int, edges [][]int) int {
+	mapNode := make(map[int][]int)
+
+	for i := 0; i < n; i ++ {
+		mapNode[i] = []int{}
+	}
+
+	for _, ed := range edges {
+		mapNode[ed[0]] = append(mapNode[ed[0]], ed[1])
+		mapNode[ed[1]] = append(mapNode[ed[1]], ed[0])
+	}
+
+	visited := make(map[int]bool)
+	var bfs func(n int)
+	bfs = func(n int) {
+		if visited[n] {
+			return
+		}
+
+		visited[n] = true
+
+		for _, node := range mapNode[n] {
+			bfs(node)
+		}
+
+		return
+	}
+
+	var res int
+
+	for i := 0; i < n; i ++ {
+		if visited[i] {
+			continue
+		}
+		bfs(i)
+		res++
+	}
+
+	return res
+}
