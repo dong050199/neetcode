ca1ec9685340077d54a99b59e017588b833a7276
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-04-07 19:58:47 +0700

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func hasCycle(head *ListNode) bool {
+	// using two pointer to detect cycle
+	first := head
+	second := head
+
+	for second.Next.Next != nil {
+		first = first.Next
+		second = second.Next.Next
+
+		if first == second {
+			return true
+		}
+	}
+
+	return false
+}
