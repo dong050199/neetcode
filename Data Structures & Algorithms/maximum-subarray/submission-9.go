c4769d20ea72ddb73aff3763a53a5ac31cd61b94
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-04-28 15:47:58 +0700

    Add: maximum-subarray - submission-9

---
+++ Data Structures & Algorithms/maximum-subarray/submission-9.go
@@ -0,0 +1,20 @@
+func maxSubArray(nums []int) int {
+	var dfs func(i int, flag bool) int
+	dfs = func(i int, flag bool) int {
+		if i >= len(nums) {
+			if flag {
+				return 0
+			}
+			return -10001
+		}
+
+		if flag {
+			return max(0 ,nums[i] + dfs(i+1, true))
+		}
+
+		return max(dfs(i+1, false), nums[i] + dfs(i+1, true))
+	}
+
+
+	return dfs(0, false)
+}
