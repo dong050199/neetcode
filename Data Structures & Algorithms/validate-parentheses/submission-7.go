e3eb0a1a85f4e5c5970504d6afd0df10adb1ba7a
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-01 14:05:30 +0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.go
@@ -0,0 +1,43 @@
+func isValid(s string) bool {
+	stack := []byte{}
+
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '{' || s[i] == '[' {
+			stack = append(stack, s[i])
+			continue
+		} 
+
+		if len(stack) == 0 {
+			return false
+		}
+
+		if s[i] == ')' {
+			cur := stack[len(stack)-1] 
+			if cur == '(' {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+			return false
+		} 
+
+		if s[i] == ']' {
+			cur := stack[len(stack)-1] 
+			if cur == '[' {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+			return false
+		} 
+
+		if s[i] == '}' {
+			cur := stack[len(stack)-1] 
+			if cur == '{' {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+			return false
+		} 
+	}
+	return true
+}
