c57c3b907fc727599f8a3e54ebb7850f3fa7504e
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-02 10:51:03 +0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.go
@@ -0,0 +1,33 @@
+func isValid(s string) bool {
+	if len(s) % 2 != 0 {
+		return false
+	}
+
+	stack := []byte{}
+
+	mp := make(map[byte]byte)
+	mp['}'] = '{'
+	mp[')'] = '('
+	mp[']'] = '['
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '{' || s[i] == '[' || s[i] == '(' {
+			stack = append(stack, s[i])
+			continue
+		}
+
+		if len(stack) == 0 {
+			return false
+		}
+
+		if _, exist := mp[s[i]]; exist {
+			cur := stack[len(stack)-1]
+			if cur == mp[s[i]] {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
