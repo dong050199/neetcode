5a3651302eafac244415435e0e6be537be8be6d9
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-03 09:01:39 +0700

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.go
@@ -0,0 +1,22 @@
+func longestConsecutive(nums []int) int {
+
+
+
+	sort.Ints(nums)
+	res := 1
+	cur := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			continue
+		}
+
+		if nums[i] == nums[i-1] + 1 {
+			cur++
+			res = max(res, cur)
+			continue
+		}
+		cur = 1
+	}
+
+	return res
+}
