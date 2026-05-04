efd32c2eed525103d92affff10ce1b22b2083c11
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-04 21:25:05 +0700

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.go
@@ -0,0 +1,26 @@
+func evalRPN(tokens []string) int {
+	first, _ := strconv.Atoi(tokens[0])
+	tokens = tokens[1:]
+	for len(tokens) > 1 {
+		second, _ := strconv.Atoi(tokens[0])
+		if tokens[1] == "+" {
+			first = first + second
+		}
+
+		if tokens[1] == "-" {
+			first = first + second
+		}
+
+		if tokens[1] == "*" {
+			first = first * second
+		}
+
+		if tokens[1] == "/" {
+			first = first / second
+		}
+
+		tokens = tokens[1:]
+	}
+
+	return first
+}
