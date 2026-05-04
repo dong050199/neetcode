c59baf9de8db594ddc7e94d3057d34b615887d9a
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-04 20:59:55 +0700

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.go
@@ -0,0 +1,25 @@
+func isValid(s string) bool {
+    stack := []byte{}
+
+	mp := make(map[byte]byte)
+	mp['}'] = '{'
+	mp[']'] = '['
+	mp[')'] = '('
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '[' || s[i] == '{' || s[i] == '(' {
+			stack = append(stack, s[i])
+			continue
+		}
+
+		if len(stack) == 0 {
+			return false
+		}
+
+		if stack[len(stack)-1] == mp[s[i]] {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}	
