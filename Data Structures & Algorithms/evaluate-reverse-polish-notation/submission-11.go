03090fa9a3ddd7fb73432304da90999566f955ae
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-04 21:47:53 +0700

    Add: evaluate-reverse-polish-notation - submission-11

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-11.go
@@ -0,0 +1,33 @@
+func evalRPN(tokens []string) int {
+	stack := []int{}
+	for _, token := range tokens {
+		if token == "*" || token == "-" || token == "/" || token == "+" {
+			second := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			first := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			var res int
+			if token == "+" {
+				res = second + first
+			}
+
+			if token == "-" {
+				res = second - first
+			}
+
+			if token == "/" {
+				res = second / first
+			}
+
+			if token == "*" {
+				res = second * first
+			}
+			stack = append(stack, res)
+
+		} else {
+			tmp , _ := strconv.Atoi(token)
+			stack = append(stack, tmp)
+		}
+	}
+	return stack[0]
+}
