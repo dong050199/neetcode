1d21b64beddde1e0c2af6755b7f5609b0e8e8758
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-09 14:45:26 +0700

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.go
@@ -0,0 +1,42 @@
+func countComponents(n int, edges [][]int) int {
+	mpNode := make(map[int][]int)
+
+	for i := 0; i < n; i++ {
+		mpNode[i] = []int{}
+	}
+
+	for _, edge := range edges {
+		mpNode[edge[0]] = append(mpNode[edge[0]], edge[1])
+		mpNode[edge[1]] = append(mpNode[edge[1]], edge[0])
+	}
+
+	visited := make([]int, n)
+	var dfs func(node, parent int) 
+
+	dfs = func(node, parent int) {
+		if visited[node] == 1 {
+			return 
+		}
+
+		visited[node] = 1
+
+		for _, n := range mpNode[node] {
+			if n == parent {
+				continue
+			}
+			dfs(n, node)
+		}
+
+		visited[node] = 2
+	} 
+
+	res := 0
+	for i := 0; i < n; i++ {
+		if visited[i] > 0 {
+			continue
+		}
+		dfs(i, -1)
+		res++
+	}
+	return res
+}
