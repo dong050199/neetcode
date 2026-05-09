b8cc81af063399c30b640b0f4aff62343b5ac97a
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-09 10:00:11 +0700

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.go
@@ -0,0 +1,34 @@
+func solve(board [][]byte) {
+    rows, cols := len(board), len(board[0])
+	directions := [][]int{{0,1},{0,-1},{1,0},{-1,0}}
+
+	var dfs func(r, c int) 
+	dfs = func(r, c int) {
+		if r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] != 'O' {
+			return 
+		}  
+
+		board[r][c] = '*'
+		for _, dir := range directions {
+			row, col := dir[0] + r, dir[1] + c
+			dfs(row, col)
+		}
+	}
+
+	for r := 1; r < rows-1; r++ {
+		for c := 1; c < cols-1; c++ {
+			if board[r][c] == 'O' {
+				dfs(r, c)
+			}
+		}
+	}
+
+
+	for r := 1; r < rows-1; r++ {
+		for c := 1; c < cols-1; c++ {
+			if board[r][c] == '*' {
+				board[r][c] = 'X'
+			}
+		}
+	}
+}
