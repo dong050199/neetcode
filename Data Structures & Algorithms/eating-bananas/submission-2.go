043780b5545d44364de8f6f6bddce5b88a1328b4
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-10 14:51:58 +0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.go
@@ -0,0 +1,30 @@
+func minEatingSpeed(piles []int, h int) int {
+	l, r := 1, maxOf(piles)
+
+	for l <= r {
+		mid := l + (r-l)/2
+		time := 0
+		for _, p := range piles {
+			if p <= mid {
+				time++
+			} else {
+				time += p / mid + 1
+			}
+		}
+
+		if time > h {
+			l = mid + 1 
+		} else {
+			r = mid - 1 
+		}
+	}
+	return l
+}
+
+func maxOf(req []int) int {
+	res := req[0]
+	for _, r := range req {
+		res = max(res, r)
+	}
+	return res
+}
