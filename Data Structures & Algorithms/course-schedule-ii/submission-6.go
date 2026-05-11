27aeee9bd81d9a15151d8b93ccda7e98d380e3f4
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-11 21:12:56 +0700

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.go
@@ -0,0 +1,47 @@
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	res := []int{}
+
+	preMap := make(map[int][]int)
+	for i := 0; i < numCourses; i++ {
+		preMap[i] = []int{}
+	}
+
+	for _, pre := range prerequisites {
+		preMap[pre[0]] = append(preMap[pre[0]], pre[1])
+	}
+
+	visited := make([]int, numCourses)
+	var dfs func(course int) bool
+	dfs = func(course int) bool {
+		if visited[course] == 1 {
+			return true
+		}
+
+		if visited[course] == 2 {
+			return false
+		} 
+
+		visited[course] = 1
+
+		for _, edge := range preMap[course] {
+			if dfs(edge) {
+				return true
+			}
+		}
+
+		visited[course] = 2
+		res = append(res, course)
+		return false
+	}
+
+	for i := 0; i < numCourses; i++ {
+		if visited[i] > 0 {
+			continue
+		}
+		if dfs(i) {
+			return []int{}
+		}
+	}
+
+	return res
+}
