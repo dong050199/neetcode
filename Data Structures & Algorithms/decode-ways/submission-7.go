49763c9dd88777299e4ae19660b6afba99fed825
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-12 21:32:50 +0700

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.go
@@ -0,0 +1,23 @@
+func numDecodings(s string) int {
+	if s[0] == '0' {
+		return 0
+	}
+
+	dp := make([]int, len(s)+1)
+	dp[0] = 1
+	dp[1] = 1
+
+	for i := 2; i <= len(s); i++ {
+		one, _ := strconv.Atoi(s[i-1:i])
+		two, _ := strconv.Atoi(s[i-2:i])
+		if one > 0 {
+			dp[i] += dp[i-1]
+		}
+
+		if two >= 10 && two <= 26 {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[len(s)]
+}
