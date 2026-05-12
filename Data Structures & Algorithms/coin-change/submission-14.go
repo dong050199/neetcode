84dc9b633ae99bdb238f04491e7437da906c45a2
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-12 21:40:13 +0700

    Add: coin-change - submission-14

---
+++ Data Structures & Algorithms/coin-change/submission-14.go
@@ -0,0 +1,21 @@
+func coinChange(coins []int, amount int) int {
+    dp := make([]int, amount+1)
+
+	for a := 0; a <= amount; a++ {
+		dp[a] = amount + 1
+	}
+
+	for a := 0; a <= amount; a++ {
+		for _, c := range coins {
+			if a - c >= 0 {
+				dp[a] = min(dp[a], dp[a-c] + 1)
+			}
+		}
+	}
+
+	if dp[amount] == amount + 1 {
+		return -1
+	}
+
+	return dp[amount]
+}
