b0a2405d7f7ae156bf93b2c1b7703519afc5f787
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-12 21:04:06 +0700

    Add: house-robber-ii - submission-9

---
+++ Data Structures & Algorithms/house-robber-ii/submission-9.go
@@ -0,0 +1,24 @@
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+    
+	return max(rob1(nums[1:]), rob1(nums[:len(nums)-1]))
+}
+
+func rob1(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	
+	dp := make([]int, len(nums)+1)
+	dp[0] = nums[0]
+	dp[1] = max(nums[1], nums[0])
+
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i - 2] + nums[i], dp[i-1])
+	}
+
+	return dp[len(nums)-1]
+}
+
