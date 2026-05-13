1aa2a3a53a6376500350bd0595f9c30051cbe401
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-13 22:19:33 +0700

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.go
@@ -0,0 +1,14 @@
+func characterReplacement(s string, k int) int {
+	l := 0
+	res := 0
+	mp := make(map[byte]bool)
+	for r := 0; r < len(s); r++ {
+		mp[s[r]] = true
+		for len(mp) > k && len(mp) != 1 {
+			delete(mp, s[l])
+			l++
+		}
+		res = max(res, r - l + 1)
+	}
+	return res
+}
