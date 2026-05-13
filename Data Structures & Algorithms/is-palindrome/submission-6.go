4d4b89fb3e6eccf0bb65766d60da23e4a17150e9
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-13 20:50:16 +0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.go
@@ -0,0 +1,28 @@
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	l, r := 0, len(s) - 1
+	for l <= r {
+		if isAlphanumeric(rune(s[r])) {
+			r--
+			continue
+		}
+
+		if isAlphanumeric(rune(s[l])) {
+			l++
+			continue
+		}
+
+		if s[l] != s[r] {
+			return false
+		}
+
+		l++
+		r--
+	}
+
+	return true
+}
+
+func isAlphanumeric(c rune) bool {
+	return unicode.IsLetter(c) || unicode.IsDigit(c)
+}
