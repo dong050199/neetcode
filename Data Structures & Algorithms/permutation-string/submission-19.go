e577a9f1a9fee2fb822c10881862a5ba61fe6099
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-15 19:14:06 +0700

    Add: permutation-string - submission-19

---
+++ Data Structures & Algorithms/permutation-string/submission-19.go
@@ -0,0 +1,25 @@
+func checkInclusion(s1 string, s2 string) bool {
+    s1Arr := [26]int{}
+    s2Arr := [26]int{}
+
+    for i := 0; i < len(s1); i++ {
+        s1Arr[s1[i] - 'a']++
+    }
+
+    l := 0
+    for r := 0; r < len(s2); r++ {
+        s2Arr[s2[r] - 'a']++
+        if r < len(s1) - 1 {
+            continue
+        }
+
+        if s1Arr == s2Arr {
+            return true
+        }  
+
+        s2Arr[s2[l] - 'a']--
+        l++
+    }
+
+    return false
+}
