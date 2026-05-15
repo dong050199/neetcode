adba30bb150f7766958d714ede1a584d06dec11b
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-15 17:51:29 +0700

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.go
@@ -0,0 +1,24 @@
+func maxSlidingWindow(nums []int, k int) []int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	q := make([]int, len(nums))
+	q[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		q[i] = max(q[i-1], nums[i])		
+	}
+
+	res := []int{}
+	for r := k - 1; r < len(nums); r++ {
+		res = append(res, q[r])
+	}
+
+    return res
+}
+
+// [1,2,1,0,4,2,6]
+// [1,2,2,2,4,4,6]
+// [1] -> 2
+// [2] -> 2 
+// [3] -> 4
+// 
