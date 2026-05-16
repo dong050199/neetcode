b886d4a21975b9d7d4c2618f9e9689d9f02d529e
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-16 14:51:05 +0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.go
@@ -0,0 +1,11 @@
+func maxProfit(prices []int) int {
+	l := 0
+	res := 0
+	for r := 0; r < len(prices); r++ {
+		if prices[r] < prices[l] {
+			l = r
+		}
+		res = max(res, prices[r] - prices[l])
+	}
+	return res
+}
