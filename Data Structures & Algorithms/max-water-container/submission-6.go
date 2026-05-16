c67c63038a3d40a2370a00ba3c43b9a54adfc0e9
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-16 14:40:07 +0700

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.go
@@ -0,0 +1,19 @@
+func maxArea(heights []int) int {
+	l, r := 0, len(heights) - 1
+	res := 0
+	maxLeft, maxRight := heights[l], heights[r]
+	for l < r {
+		res = max(res , (r - l) * min(maxLeft, maxRight))
+		
+		if maxLeft > maxRight {
+			r--
+		} else {
+			l++
+		}
+
+		maxRight = max(maxRight, heights[r])
+		maxLeft = max(maxLeft, heights[r])
+	}
+
+	return res
+}
