a3483beda9e54bc44c44869db6de72c4f55ad045
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-16 13:09:36 +0700

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.go
@@ -0,0 +1,18 @@
+func dailyTemperatures(temperatures []int) []int {
+	stack := []int{}
+	res := make([]int, len(temperatures))
+
+	for i := len(temperatures) - 1; i >= 0; i-- {
+		for len(stack) > 0 && temperatures[i] >= temperatures[stack[len(stack)-1]] {
+			stack = stack[:len(stack)-1]
+		}
+
+		if len(stack) > 0 {
+			res[i] = stack[len(stack)-1] - i
+		}
+
+		stack = append(stack, i)
+	}
+
+	return res
+}
