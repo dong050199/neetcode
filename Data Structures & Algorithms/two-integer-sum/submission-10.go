c557ad1e48f684b40bb9006a7ca311d56a4cf6ee
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-16 13:20:49 +0700

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.go
@@ -0,0 +1,18 @@
+func twoSum(nums []int, target int) []int {
+    mp := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		mp[nums[i]] = i
+	}
+
+	for i, num := range nums {
+		diff := target - num
+		if val, exist := mp[diff]; exist {
+			if i == val {
+				continue
+			}
+			return []int{i, val}
+		}
+	}
+
+	return []int{}
+}
