c7bb927b27eada374dfbcc778d229b6311619873
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-16 14:59:49 +0700

    Add: longest-repeating-substring-with-replacement - submission-13

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-13.go
@@ -0,0 +1,18 @@
+func characterReplacement(s string, k int) int {
+	mp := make(map[byte]int)
+	maxf := 0
+	l := 0
+	res := 0
+	for r := 0; r < len(s); r++ {
+		mp[s[r]]++
+		maxf = max(maxf, mp[s[r]])
+
+		for (r - l + 1) - maxf > k {
+			l++
+		}
+
+		res = max(res, r - l + 1)
+	}
+
+	return res
+}
