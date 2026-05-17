a2894ce01442d5c25927db391da055e3fc09b14c
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-17 13:17:58 +0700

    Add: reorder-linked-list - submission-4

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-4.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reorderList(head *ListNode) {
+    var nodes []*ListNode
+	cur := head
+
+	for cur != nil {
+		nodes = append(nodes, cur)
+		cur = cur.Next
+	}
+
+	l, r := 0, len(nodes) - 1
+
+	for l < r {
+		nodes[l].Next = nodes[r]
+		l++
+		nodes[r].Next = nodes[l]
+		r--
+	}
+
+	nodes[l].Next = nil
+
+	return 
+}
