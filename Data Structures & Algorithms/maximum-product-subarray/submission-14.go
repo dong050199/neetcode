9239fa807e7df17f6efb07970f48347e960f2b52
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-17 21:37:54 +0700

    Add: maximum-product-subarray - submission-14

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-14.go
@@ -0,0 +1,11 @@
+func maxProduct(nums []int) int {
+	res := 1
+	maxProd, minProd := 1, 1
+	for i := 0; i < len(nums); i++ {
+		curMax, curMin := maxProd, minProd
+		maxProd = max(curMax * nums[i], curMin * nums[i], nums[i])
+		minProd = min(curMax * nums[i], curMin * nums[i], nums[i])
+		res = max(res, maxProd)
+	}
+	return res
+}
