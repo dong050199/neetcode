56ed992ac34806a66e9883ff7dd4616d8eee1d52
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-17 14:54:34 +0700

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if isSameTree(root, subRoot) {
+		return true
+	} 
+	return isSameTree(root.Left, subRoot) || isSameTree(root.Right, subRoot) 
+}
+
+func isSameTree(tree1, tree2 *TreeNode) bool {
+	if tree1 == nil && tree2 == nil {
+		return true
+	}
+
+	if tree1 == nil || tree2 == nil {
+		return false
+	}
+
+	if tree1.Val != tree2.Val {
+		return false
+	}
+
+	return isSameTree(tree1.Left, tree2.Left) && isSameTree(tree1.Right, tree2.Right)
+}
