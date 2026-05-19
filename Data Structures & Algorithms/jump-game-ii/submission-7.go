058dd2ce51cf7724b2062018e80759be21451e53
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-19 20:45:28 +0700

    Add: jump-game-ii - submission-7

---
+++ Data Structures & Algorithms/jump-game-ii/submission-7.go
@@ -0,0 +1,16 @@
+func jump(nums []int) int {
+    maxJump := 0
+	res := 0
+	for i, num := range nums {
+		if i + num > maxJump {
+			maxJump = i + num 
+			if i == len(nums) - 1 {
+				if maxJump >= 0 {
+					return res
+				}
+			}
+			res++
+		}		
+	}
+	return res
+}
