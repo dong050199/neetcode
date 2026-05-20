c1b05c399f13b18ba984265839d4ecb218b5f23d
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-20 23:28:13 +0700

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.go
@@ -0,0 +1,20 @@
+func lengthOfLIS(nums []int) int {
+    dp := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+	}
+
+	res := 1
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[i], dp[j] + 1)
+			} 
+
+			res = max(res, dp[i])
+		}
+	}
+
+	return res
+}
