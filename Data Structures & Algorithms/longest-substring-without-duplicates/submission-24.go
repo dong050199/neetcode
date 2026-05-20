06ba35717489f532e84b80286586e36810440d7c
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-20 23:36:07 +0700

    Add: longest-substring-without-duplicates - submission-24

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-24.go
@@ -0,0 +1,21 @@
+func lengthOfLongestSubstring(s string) int {
+	res := 0
+	l := 0
+	mp := make(map[byte]int)
+	for r := 0; r < len(s); r++ {
+		if _, exist := mp[s[r]]; !exist {
+			mp[s[r]] = r
+			res = max(res, r - l + 1)
+			continue
+		}
+
+		for l <= mp[s[r]] {
+			delete(mp, s[l])
+			l++
+		}
+
+		mp[s[r]] = r
+	}
+
+	return res
+}
