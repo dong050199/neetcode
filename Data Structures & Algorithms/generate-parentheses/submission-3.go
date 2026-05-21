1e41304c40c1b289a49090480e6685ae9b964813
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-21 18:24:07 +0700

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.go
@@ -0,0 +1,22 @@
+func generateParenthesis(n int) []string {
+	res := []string{}
+
+	var dfs func(i, j int, cur string)
+	dfs = func(i, j int, cur string) {
+		if i == j && i == n {
+			res = append(res, cur)
+		}
+
+		if i < n {
+			dfs(i + 1, j, cur + "(")
+		}
+
+		if j < i {
+			dfs(i, j + 1, cur + ")")
+		}
+	}
+
+	dfs(0, 0, "")
+
+	return res
+}
