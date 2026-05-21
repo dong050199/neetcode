192e3b88e167ba1a184174c88eea5866ddac1202
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-05-21 19:52:34 +0700

    Add: trapping-rain-water - submission-8

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-8.go
@@ -0,0 +1,24 @@
+func trap(height []int) int {
+	l, r := 0, len(height) - 1
+	maxLeft, maxRight := height[l], height[r]
+	res := 0
+	for l < r {
+		if maxLeft < maxRight {
+			l++
+			if maxLeft >  height[l] {
+				res +=  maxLeft - height[l]
+			} else {
+				maxLeft = height[l] 
+			}	
+		} else {
+			r--
+			if maxRight > height[r] {
+				res += maxRight - height[r]
+			} else {
+				maxRight = height[r]
+			}
+		}
+
+	}
+	return res
+}
