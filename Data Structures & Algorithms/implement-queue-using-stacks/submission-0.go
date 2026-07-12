ef626dc9c8bfde5f2edfa3ff9b9c94d7a0b2ae5e
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-07-13 06:52:55 +0700

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.go
@@ -0,0 +1,53 @@
+type MyQueue struct {
+	s1 []int
+	s2 []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{
+		s1: []int{},
+		s2: []int{},
+	}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.s1 = append(this.s1, x)
+}
+
+func (this *MyQueue) Pop() int {
+	if len(this.s2) == 0 {
+		for len(this.s1) > 0 {
+			cur := this.s1[len(this.s1)-1]
+			this.s1 = this.s1[:len(this.s1)-1]
+			this.s2 = append(this.s2, cur)
+		}
+	}
+	res := this.s2[len(this.s2)-1]
+	this.s2 = this.s2[:len(this.s2)-1]
+	return res
+}
+
+func (this *MyQueue) Peek() int {
+	if len(this.s2) == 0 {
+		for len(this.s1) > 0 {
+			cur := this.s1[len(this.s1)-1]
+			this.s1 = this.s1[:len(this.s1)-1]
+			this.s2 = append(this.s2, cur)
+		}
+	}
+	res := this.s2[len(this.s2)-1]
+	return res
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.s2) == 0 && len(this.s1) == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
