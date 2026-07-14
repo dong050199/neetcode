e5f5048b26cb3b65c3d5f9bb31a5ad51b70750fc
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-07-14 08:05:41 +0700

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.go
@@ -0,0 +1,10 @@
+func containsDuplicate(nums []int) bool {
+    mp := make(map[int]bool)
+    for _, num := range nums {
+        if mp[num] {
+            return true
+        }
+        mp[num] = true
+    }
+    return false
+}
