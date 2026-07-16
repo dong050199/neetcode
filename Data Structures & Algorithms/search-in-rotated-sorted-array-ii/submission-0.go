df7b59439be92f191b210e3dfa4c61474e8e3f65
Tran Dong <82258715+dong050199@users.noreply.github.com>
2026-07-16 08:08:58 +0700

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.go
@@ -0,0 +1,40 @@
+func search(nums []int, target int) bool {
+	// this problem must be solve by using binary search, for sure
+	// first we need to find peak of the array where the order of array was changed
+	l, r := 0, len(nums)-1
+	for l < r {
+		mid := l + (r - l) / 2
+		// [2,3,4,5,6,1] 
+		// [4,5,6,1,2,3] 
+		if nums[mid] >= nums[r] {
+			l = mid
+		} else {
+			r = mid - 1
+		}
+	}
+
+	peak := l
+	// now we have two sub arr
+	if nums[0] > target {
+		l = peak + 1
+		r = len(nums) - 1
+	} else {
+		l = 0
+		r = peak
+	}
+
+	for l <= r {	
+		mid := l + (r - l)/2
+		if nums[mid] == target {
+			return true
+		}
+
+		if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid +1
+		}
+	}
+
+	return false
+}
